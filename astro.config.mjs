97e75ab99efc4b099e4d1d82251e5c7b6230f0f8
VeldtJumper <veldtjumper@gmail.com>
2026-04-22 01:38:55 -0500

    feat: hijack ⌘F/Ctrl+F to focus search, fix emoji name overflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,9 @@ export default defineConfig({
   site: 'https://emoji.observer',
   base: '/',
   output: 'static',
+  vite: {
+    server: {
+      allowedHosts: ['grindavik', '192.168.86.155', '100.82.99.58'],
+    },
+  },
 });
